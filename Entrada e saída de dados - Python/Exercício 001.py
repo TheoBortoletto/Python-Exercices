acf41a8cc0fe808467be4d133d49bee630f04061
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-07 10:15:45 -0300

    Exercice 001

--- Entrada e saída de dados - Python/Exercício 001.py
+++ Entrada e saída de dados - Python/Exercício 001.py
@@ -2,3 +2,13 @@ print("\n\t - Exercício 1 \n")
 
 #* 1) Ler 4 valores, calcular a média entre eles e apresentar o resultado:
 
+val1 = float (input("Digite o valor do produto 1: "))
+val2 = float (input("Digite o valor do produto 2: "))
+val3 = float (input("Digite o valor do produto 3: "))
+val4 = float (input("Digite o valor do produto 4: "))
+
+print("\n")
+
+media = (val1 + val2 + val3 + val4) / 4 
+
+print("A média destes produtos é: %.2f" %media, "\n")
