e6c847c25deb4f830adfbff0adfde58013937e2c
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-13 15:27:33 -0300

    add ex 01

---
+++ Python Brasil/Estrutura de Decisão/Ex.01.py
@@ -0,0 +1,13 @@
+print("\n\t Exercício 1 \n")
+
+'''Faça um Programa que peça dois números e imprima o maior deles'''
+
+num1 = float (input("Digite um número: "))
+num2 = float (input("Digite mais um número: "))
+print("\n")
+
+if num1 > num2:
+    print("O número", num1, "é maior que o número", num2,"\n")
+
+else:
+    print("O número", num2, "é maior que o número", num1,"\n")
