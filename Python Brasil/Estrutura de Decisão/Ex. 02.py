31d51e10c4257fd4124508d4f83fdb3d3725496b
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-13 15:32:48 -0300

    add contetnt

---
+++ Python Brasil/Estrutura de Decisão/Ex. 02.py
@@ -0,0 +1,11 @@
+print("\n\t Exercício 2 \n")
+
+'''Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+
+num  = int (input("Digite um número: "))
+
+if num >= 0:
+    print("O número", num, "é positivo. \n")
+
+else:
+    print("O número", num, "é negativo. \n")
