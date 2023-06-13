7ef6b0d4f1873f7b89632aa6ededb35955095a13
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-13 15:51:22 -0300

    fix exercise

--- Python Brasil/Estrutura de Decisão/Ex. 03.py
+++ Python Brasil/Estrutura de Decisão/Ex. 03.py
@@ -1,4 +1,15 @@
 print("\n\t Exercício 3 \n")
 
 '''Faça um Programa que verifique se uma letra digitada é 
-    "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.'''
+   "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.'''
+
+string = str (input("Digite 'F' para feminino e 'M' para masculino: "))
+
+if string == "f" or str == "F":
+    print("Feminino. \n")
+
+if string == "m" or str == "M":
+    print("Masculino. \n")
+
+else:
+    print("Sexo Inválido. \n")
