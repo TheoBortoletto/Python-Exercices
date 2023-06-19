922c4630dd61f9d412bee15dc741f52115a4d8b1
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-19 10:43:36 -0300

    fixed exercise

--- Python Brasil/Estrutura de Decisão/Ex. 17.py
+++ Python Brasil/Estrutura de Decisão/Ex. 17.py
@@ -3,10 +3,9 @@ print("\n\t Exercício 17")
 '''Faça um Programa que peça um número correspondente a um determinado
  ano e em seguida informe se este ano é ou não bissexto.'''
 
-num = int (input("Digite um ano para saber se ele é bissexto ou não: "))
+ano = int (input("Digite um ano para saber se ele é bissexto ou não: "))
 
-ano = int(input('Ano: '))
 if (ano%4==0 and ano%100!=0) or (ano%400==0):
-    print('Bissexto')
+    print('Bissexto\n')
 else:
-    print('Não é bissexto')
+    print('Não é bissexto\n')
