8743aa83d27b93edfb3801432b41ab7f837539c7
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-20 10:52:12 -0300

    add exercise

--- Python Brasil/Exercício com Listas/Ex. 02.py
+++ Python Brasil/Exercício com Listas/Ex. 02.py
@@ -1,1 +1,14 @@
 print("\n\t Exercício 2 \n")
+
+'''Faça um Programa que leia um vetor de 10 números 
+reais e mostre-os na ordem inversa.'''
+
+num = []
+i = 1
+
+while i <= 10:
+    num.append(int (input("Digite um número: ")))
+    i += 1
+
+num.sort(reverse = True)
+print("\nOrdem inversa dos valores digitados: ", num, "\n")
