70ea7585b2c16aebcc3be7fc2a166ea19ee87868
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-20 10:43:34 -0300

    add files & exercises

---
+++ Python Brasil/Exercício com Listas/Ex. 01.py
@@ -0,0 +1,13 @@
+print("\n\t Exercício 1 \n")
+
+'''Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.'''
+
+num = []
+i = 1
+
+while i <= 5:
+    num.append (int (input("Digite um valor: ")))
+    i += 1
+
+print("\n",num, "\n")
+
