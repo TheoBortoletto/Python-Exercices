743047cd51637c2e61c44505d94737a1c69d52f8
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-20 15:56:45 -0300

    unsolved exercise

---
+++ Python Brasil/Exercício com Listas/Ex. 12.py
@@ -0,0 +1,14 @@
+print("\n\t Exercício 12 \n")
+
+'''Foram anotadas as idades e alturas de 30 alunos. 
+Faça um Programa que determine quantos alunos com mais 
+de 13 anos possuem altura inferior à média de altura desses alunos.
+'''
+
+idade = []
+altura = []
+cont = 1
+
+while cont <= 5:
+    print("Aluno:", cont+ '':')
+    idade.append(int (input("Informe a sua idade: ")))
