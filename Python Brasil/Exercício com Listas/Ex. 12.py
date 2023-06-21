c9f302222600416de6479f196ed98c35356d9edd
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-21 11:43:31 -0300

    fixing exercise 12

--- Python Brasil/Exercício com Listas/Ex. 12.py
+++ Python Brasil/Exercício com Listas/Ex. 12.py
@@ -5,32 +5,38 @@ Faça um Programa que determine quantos alunos com mais
 de 13 anos possuem altura inferior à média de altura desses alunos.
 '''
 
-idade = []
-altura = []
-idadeMaiorTreze = []
+import random
+idades = []
+alturas = []
+aluno_13 = []
+media_13 = []
 
-perguntaI = []
-perguntaA = []
+for i in range(30):
+    numero_aleatorio = idades.append(random.randrange(1, 20)) #! Números aleatórios 
+                                                              #! de 1 a 20 (idades).
 
-cont = 1
+    numero_aleatorio2 = alturas.append(random.randrange(150, 200)) #! Números aleatórios 
+                                                                  #! de 50 a 200 (altura 
+                                                                  #! em cm).
+
+for i in range(30):
+    if idades[i] > 13: #! Percorre o vetor das 
+                       #! idades e verifica se as idades são maiores que 13.
+
+        aluno_13.append(alturas[i]) #! Se a condição for verdadeira, 
+                                    #! pega as alturas dos que são maiores de 13.
+
+print("\nAltura dos alunos que são maiores de 13 anos: ", aluno_13) 
+
+media = sum(aluno_13) / len(aluno_13) #! Faz a soma das alturas e
+                                      #! divide ela pela quantidade 
+                                      #! de itens que tem dentro dessa lista
 
 
-
-
-while cont <= 5:
-    print("Aluno", cont,":")
-    perguntaI.append(int (input("Informe a sua idade: ")))
-    perguntaA.append(float (input("Informe a sua altura: ")))
-    cont += 1
-
-mediaAltura = sum(perguntaA) / 5
-   
-if perguntaA < mediaAltura:
-    idadeMaiorTreze.append(perguntaA)
-
-
-print(idadeMaiorTreze)
-
-
+for i in range(len(aluno_13)): #! Percorre o tanto de itens que contém nessa lista
+                               #! Nesta lista só há as alturas dos que tem mais de 13
+                               
+    if aluno_13[i] < media: #!
+        media_13.append(aluno_13[i])
 
 
