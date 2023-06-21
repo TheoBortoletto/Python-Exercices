e4e992743a59501c9204f69476c78d6d29c890a6
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-21 12:52:24 -0300

    add exercise

---
+++ Python Brasil/Exercício com Listas/Ex. 13.py
@@ -0,0 +1,21 @@
+print("\n\t Exercício 13 \n")
+
+'''Faça um programa que receba a temperatura média de cada 
+mês do ano e armazene-as em uma lista. Após isto, calcule 
+a média anual das temperaturas e mostre todas as temperaturas 
+acima da média anual, e em que mês elas ocorreram 
+(mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro, . . . ).'''
+
+meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+temperaturas = []
+cont = 0
+i = 0
+
+for mes in meses:
+    temperatura = float (input(f"Informe a temperatura do mês de {mes}: "))
+
+temperaturas.append(temperatura)
+    
+media = sum(temperaturas) / len(temperaturas)
+
+print(media)
