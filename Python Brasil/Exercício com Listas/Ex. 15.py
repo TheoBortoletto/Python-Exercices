d23f464ce3c0be4043bb9a1ce3ba36e9355ed58c
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-22 09:32:13 -0300

    add content

--- Python Brasil/Exercício com Listas/Ex. 15.py
+++ Python Brasil/Exercício com Listas/Ex. 15.py
@@ -9,7 +9,7 @@ Mostre a quantidade de valores que foram lidos; OK
 Exiba todos os valores na ordem em que foram informados, um ao lado do outro; OK
 Exiba todos os valores na ordem inversa à que foram informados, um abaixo do outro; OK
 Calcule e mostre a soma dos valores; OK
-Calcule e mostre a média dos valores;
+Calcule e mostre a média dos valores; OK
 Calcule e mostre a quantidade de valores acima da média calculada;
 Calcule e mostre a quantidade de valores abaixo de sete;
 Encerre o programa com uma mensagem;'''
@@ -19,7 +19,7 @@ numeros = []
 cont = 0
 
 while cont > -1:
-    numUser = float (input("Digite a sua nota: "))
+    numUser = float (input("Digite a sua nota ou '-1' para encerrar: "))
     numeros.append(numUser)
     if numUser == -1:
         numeros.pop()
@@ -27,11 +27,24 @@ while cont > -1:
     else:
         cont += 1
 
-print("\nQuantidade de valores que foram lidos: ", len(numeros))
+print("\nQuantidade de valores que foram lidos: ", len(numeros), "\n")
+
 print("Valores na ordem que foram digitados, um do lado do outro: ", numeros, "\n")
+
 numeros.reverse()
-print("Valores na ordem inversa à que foram informados, um abaixo do outro: ", *numeros, sep = "\n")
+
+print("Valores na ordem inversa à que foram informados, um abaixo do outro: \n", *numeros, sep = "\n")
+
 numeros.reverse()
+
+print("\nValores da lista somados: ", sum(numeros), "\n")
+
+media = sum(numeros) / len(numeros)
+
+print("Médias dos valores: ", media, "\n")
+
+
+
 
 
 
