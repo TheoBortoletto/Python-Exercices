b778d5ff1361b0db3ca22a01604aefc16ecb618b
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-27 09:40:58 -0300

    add content

--- Exercício Théo/User register.py
+++ Exercício Théo/User register.py
@@ -1,5 +1,10 @@
 print("\n\t\t [--------------- Novo Usúario ---------------]\n")
 
+print("\t Registro: \n")
 email = str (input("Digite o seu E-mail: "))
 senha = str (input("Digite a sua Senha: "))
+
+
+
+
 
