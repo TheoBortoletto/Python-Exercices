7ef9c46e4a643153500fac8b4feac4ebd9bb6709
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-27 13:01:46 -0300

    add file

---
+++ Exercícios (rmveiga)/Básico/Datas/Ex. 02.py
@@ -0,0 +1,9 @@
+print("\n\t Exercício 2 \n")
+
+'''Carregue a data atual do computador e apresente somente a data'''
+
+from datetime import datetime
+
+data = datetime.now()
+
+print("Data atual: ", data.date(), "\n")
