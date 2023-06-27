c50545afcc6827e3d6267cd26f1a509207748810
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-27 09:46:43 -0300

    add content

--- Exercício Théo/User register.py
+++ Exercício Théo/User register.py
@@ -4,7 +4,14 @@ print("\t Registro: \n")
 email = str (input("Digite o seu E-mail: "))
 senha = str (input("Digite a sua Senha: "))
 
+print("\n")
 
+confirmEmail = str (input("Confirme o seu E-mail: "))
+confirmSenha = str (input("Confirme a sua Senha: "))
 
+print("\n")
 
-
+while confirmEmail != email or confirmSenha != senha:
+    print("\t O")
+    
+    
