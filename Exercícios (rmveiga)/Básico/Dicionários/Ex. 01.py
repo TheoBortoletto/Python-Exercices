b6dbfb8a3533e9c7184693d1854a4995f018d0df
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-28 09:30:54 -0300

    add file

--- Exercícios (rmveiga)/Básico/Dicionários/Ex. 01.py
+++ Exercícios (rmveiga)/Básico/Dicionários/Ex. 01.py
@@ -3,3 +3,10 @@ print("\n\t Exercício 1 \n")
 '''Considere o seguinte dicionário: {'m1': {'m2': 'Olá Mundo'}}. 
 Carregue e apresente a mensagem "Olá Mundo" contida no dicionário
 '''
+
+dict = {
+    'm1': {'m2': 'Olá Mundo'}
+}
+
+print(dict["m1"]["m2"], "\n")
+
