f44d0840f82128c05b999c3a5e1a133a8f420398
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 15:22:44 -0300

    add exercise

---
+++ Exercícios ( Git-Hub rmveiga)/Básico/Mensagens/Ex. 01.py
@@ -0,0 +1,4 @@
+print("\n\t Exercício 1 \n")
+
+'''Desenvolva um programa que apresente 
+na tela a seguinte mensagem: Hello World'''
--- Exercícios ( Git-Hub rmveiga)/Básico/Mensagens/Ex. 02.py
+++ Exercícios ( Git-Hub rmveiga)/Básico/Mensagens/Ex. 02.py
@@ -1,1 +1,4 @@
-print("\n\t Exercício 1 \n")
+print("\n\t Exercício 2 \n")
+
+'''Desenvolva um programa que pergunte o seu nome e, 
+ao teclar Enter, apresente uma saudação personalizada'''
