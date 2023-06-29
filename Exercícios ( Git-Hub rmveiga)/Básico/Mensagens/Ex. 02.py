caedff2678aa735dd8d303d0d3102001ab741627
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 15:25:18 -0300

    add exercise

--- Exercícios ( Git-Hub rmveiga)/Básico/Mensagens/Ex. 02.py
+++ Exercícios ( Git-Hub rmveiga)/Básico/Mensagens/Ex. 02.py
@@ -2,3 +2,7 @@ print("\n\t Exercício 2 \n")
 
 '''Desenvolva um programa que pergunte o seu nome e, 
 ao teclar Enter, apresente uma saudação personalizada'''
+
+nome = str (input("Informe o seu nome: "))
+
+print(f"\nBem-vindo {nome}!!!\n")
