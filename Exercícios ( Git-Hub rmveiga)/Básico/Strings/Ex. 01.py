407cf05b93bf8fc1800264f401672aeccb8cb04c
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 15:50:38 -0300

    add exercise

---
+++ Exercícios ( Git-Hub rmveiga)/Básico/Strings/Ex. 01.py
@@ -0,0 +1,12 @@
+print("\n\t Exercício 1 \n")
+
+'''Desenvolva um programa que altere em tempo 
+de execução a palavra Java pela palavra Python 
+na frase Exercícios de Java
+'''
+
+txt = "Exercícos de Java"
+
+x = txt.replace("Java", "Pyhton")
+
+print(x, "\n")
