bc641d8acf5b43ce1c18f9bc0907f73288ca1c34
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 13:17:24 -0300

    add content

--- Exercícios ( Git-Hub rmveiga)/Básico/Listas/Ex. 01.py
+++ Exercícios ( Git-Hub rmveiga)/Básico/Listas/Ex. 01.py
@@ -5,4 +5,16 @@ quatro notas em uma lista e que apresente:
 média final, a maior nota e a menor nota'''
 
 notas = []
+cont = 1
 
+while cont <= 4:
+    Notas = float (input("Informe a sua nota: "))
+    notas.append(Notas)
+    cont += 1
+    
+media = sum(notas) / 4
+
+print("\nNotas: ", notas, "\n")
+print("Média: ", media)
+print("Nota maior: ", max(notas))
+print("Menor nota: ", min(notas), "\n")
