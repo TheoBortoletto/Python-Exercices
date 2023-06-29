509d7ade0996a44293b4a625c1552e87c4606bf2
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 15:33:20 -0300

    add content

---
+++ Exercícios ( Git-Hub rmveiga)/Básico/Números/Ex. 01.py
@@ -0,0 +1,9 @@
+print("\n\t Exercício 1 \n")
+
+'''Desenvolva um programa que leia um número inteiro 
+qualquer e que apresente o número informado, seguido 
+do seu antecessor e do seu sucessor'''
+
+num = int (input("Informe um número: "))
+
+print(f"Número digitado: {num};\nAntecessor do número digitado: {num-1};\nSucessor do número digitado: {num+1}. \n")
