9a4bc2913ff59cd1a28632bdc9e7a5d0b3f7d005
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 12:57:33 -0300

    add content

--- Exercícios ( Git-Hub rmveiga)/Básico/Funções/Ex. 01.py
+++ Exercícios ( Git-Hub rmveiga)/Básico/Funções/Ex. 01.py
@@ -1,6 +1,16 @@
 print("\n\t Exercício 1 \n")
 
-'''Desenvolva um programa que tenha uma 
+'''Desenvolva um programa que tenha uma função
 que verifique se um número inteiro qualquer 
 é par ou impar'''
 
+def parImpar():
+    if num %2 == 0:
+        print("\nO número", num, "é par. \n")
+    else:
+        print("\nO número", num, "é ímpar. \n")
+    
+
+num = int (input("Informe um número para ver se ele é Par ou Ímpar: "))
+
+parImpar()
