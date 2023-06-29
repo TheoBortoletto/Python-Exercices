30bd469b48e6521a3329cfee326e72b6c84083e5
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-29 15:36:02 -0300

    add exercise

---
+++ Exercícios ( Git-Hub rmveiga)/Básico/Números/Ex. 02.py
@@ -0,0 +1,8 @@
+print("\n\t Exercício 2 \n")
+
+'''Desenvolva um programa que leia um número inteiro 
+qualquer e que apresente o número informado com duas casas decimais'''
+
+num = int (input("Digite um número: "))
+
+print("%.2f"  % num, "\n")
