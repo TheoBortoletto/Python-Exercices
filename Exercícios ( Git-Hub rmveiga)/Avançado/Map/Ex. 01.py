bf539ff285c875abd08ad48f70ce5fef04af79c3
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-03 14:44:47 -0300

    add exercise

--- Exercícios ( Git-Hub rmveiga)/Avançado/Map/Ex. 01.py
+++ Exercícios ( Git-Hub rmveiga)/Avançado/Map/Ex. 01.py
@@ -1,2 +1,10 @@
 print("\n\t Exercício 1 \n")
 
+'''Desenvolva um programa que converta todas as temperaturas 
+desta lista em Celsius: ([22.5, 40, 13, 29, 34]) para Fahrenheit
+'''
+
+tempC = [22.5, 40, 13, 29, 34]
+
+tempF = list(map(lambda temp: round((9/5) * temp + 32, 1), tempC))
+print(tempF, "\n")
