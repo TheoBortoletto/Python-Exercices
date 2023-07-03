e7aab22748f6d6ba10234f1a88a9b32aa65c1d11
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-03 11:03:41 -0300

    add exercise

--- Exercícios ( Git-Hub rmveiga)/Avançado/Lambda/Ex. 02.py
+++ Exercícios ( Git-Hub rmveiga)/Avançado/Lambda/Ex. 02.py
@@ -1,2 +1,15 @@
 print("\n\t Exercício 2 \n")
 
+'''Desenvolva um programa que leia um número qualquer e informe se ele é par ou ímpar'''
+
+num = int (input("Informe um número: "))
+print("\n")
+
+par = lambda a: num % 2 == 0
+
+if par(num):
+    print(f"O número {num} é par. \n")
+else:
+    print(f"O número {num} é ímpar. \n")
+
+#* print(par(num), "\n")
