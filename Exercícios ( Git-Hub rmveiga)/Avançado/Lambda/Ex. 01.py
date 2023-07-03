4e4772116ff498592cb3d0cc64b4393fe629623d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-03 10:44:39 -0300

    add exercise

--- Exercícios ( Git-Hub rmveiga)/Avançado/Lambda/Ex. 01.py
+++ Exercícios ( Git-Hub rmveiga)/Avançado/Lambda/Ex. 01.py
@@ -1,2 +1,15 @@
-print("\n\t Exercício 1")
+print("\n\t Exercício 1 \n")
+
+'''Desenvolva um programa que leia o seu nome 
+completo e que apresente somente o seu primeiro 
+e último nomes
+'''
+
+fulln = str (input("Informe o seu nome completo: "))
+print("\n")
+fname = lambda nome: nome.split()[0] #! Pegará a primeira posição -> (primeiro nome).
+lname = lambda nome: nome.split()[-1] #! Pegará a primeira posição de trás pra frente.
+
+print(f"Seu primeiro nome é {fname(fulln)}.")
+print(f"Seu último nome é {lname(fulln)}. \n")
 
