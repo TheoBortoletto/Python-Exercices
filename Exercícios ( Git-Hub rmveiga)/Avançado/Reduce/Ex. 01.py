2a25ec4bc1ad59966936a81b229a1489bbb4b0ae
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-03 15:57:16 -0300

    fix exerceise

--- Exercícios ( Git-Hub rmveiga)/Avançado/Reduce/Ex. 01.py
+++ Exercícios ( Git-Hub rmveiga)/Avançado/Reduce/Ex. 01.py
@@ -1,4 +1,37 @@
 print("\n\t Exercício 1 \n")
 
+'''Desenvolva um programa que apresente o 
+maior e o menor valores da sequência ([54, 10, 29, 87, 7, 64])
+'''
 
+#! ERRADO:
 
+#* from functools import reduce
+
+#* list = [54, 10, 29, 87, 7, 64]
+
+#* def acharMin(x, y):
+#*     if x < y:
+#*         return x
+#*     else:
+#*         return y
+    
+#* def acharMax(x, y):
+#*     if x > y:
+#*         return y
+#*     else:
+#*         return x
+    
+#* minimo = reduce(acharMin, list)
+#* maximo = reduce(acharMax, list)
+
+#* print(f"O menor valor da lista é: {minimo}.")
+#* print(f"O maior valor da lista é: {maximo}. \n")
+
+#! CERTO:
+
+from functools import reduce
+
+list = [54, 10, 29, 87, 7, 64] 
+
+maiorValor = 
